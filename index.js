fb6401e434f13eb767f421f3e07d2cb3c6b2df4f
Rahul Kumar <33459576+RahulkumarRV@users.noreply.github.com>
2022-10-22 14:14:08 +0000

    file uploaded complete

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var multer = require('multer');
 var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +13,26 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname)
+  }
+})
+
+const upload = multer({storage: storage });
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 
 
